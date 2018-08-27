37008eccd200c4bc303d2b6628bff500b165c042
Charles Ledvina <charles@indexdata.com>
2018-08-27 15:42:45 -0500

    Dislplay ontologies into grid.

--- source/assets/js/modules/profile/controllers/ontologies.controller.js
+++ source/assets/js/modules/profile/controllers/ontologies.controller.js
@@ -4,48 +4,56 @@
  * @description
  * Handles the ontologies list page. Gets ontologies from the server and populates
  * the list with them. Also contains functions for sorting and searching the
- * profile list
+ * ontologies list
  */
 angular.module('locApp.modules.profile.controllers')
     .controller('ontologiesController', function($scope, $state, $stateParams, Server) {
         $scope.numItems = 10;
-        $scope.profiles = [];
+        $scope.ontologies = [];
         $scope.searchText = "";
 
-        Server.get('/verso/api/configs?filter[where][configType]=profile', {})
+        Server.get('/verso/api/configs?filter[where][configType]=vocabulary&filter[where][name][neq]=Languages', {})
             .then(function(response) {
                 for(var i = 0; i < response.length; i++) {
                     // Logic to format the date correctly
-                    var record = response[i].json;
-                    var parse = (record.Profile.date) ? record.Profile.date.split('-') : [];
-                    var date = (parse.length > 0) ? new Date(parse[0], parse[1] - 1, parse[2]) : new Date();
-                    record.Profile.date = date.toDateString().slice(4, date.length);
-                    record.Profile.versoId = response[i].id;
-
-                    // push profile to list
-                    $scope.profiles.push(record.Profile);
+                    var oSource = response[i];
+                    var name = oSource.name;
+                    oSource.json.RDF.Class.forEach(function(oClass) {
+                        oClass.oType = 'Resource'
+                        oClass.source = name;
+                        $scope.ontologies.push(oClass);
+                    });
+                    if (oSource.json.RDF.Property !== undefined) {
+                        oSource.json.RDF.Property.forEach(function(oProp) {
+                            oProp.oType = 'Property';
+                            oProp.source = name;
+                            $scope.ontologies.push(oProp);
+                        });
+                    }
                 }
-                $scope.sortByParam('title', 'asc');
                 $scope.addBlanks();
             });
-
+            console.log($scope.ontologies);
         /**
          * Defines the table parameters for use in the ui-grid
          */
-        $scope.profileGrid = {
-            data: "profiles",
+
+        $scope.ontologiesGrid = {
+            data: "ontologies",
             columnDefs: [
                 {
-                    field:'title', displayName:'Title', width: 160,
-                    cellTemplate: '<a href="#/profile/{{row.entity.versoId}}"  class="pad-cell">{{ row.entity.title }}</a>'
+                    field:'source', displayName:'Source', width: 120
                 },
                 {
-                    field:'description', displayName:'Description'
+                    field:'label.__text', displayName:'Label', width: 160,
+                    cellTemplate: '<a href="#/ontologies/{{row.entity.versoId}}"  class="pad-cell">{{ row.entity.label.__text }}</a>'
                 },
                 {
-                    field:'date', displayName:'Date', width: 120,
-                    enableSorting: false
-                }
+                    field:'oType', displayName:'Type', width: 80
+                },
+                {
+                    field:'comment.__text', displayName:'Description'
+                },
             ],
             enableHorizontalScrollbar: false,
             enableVerticalScrollbar: false
@@ -58,7 +66,7 @@ angular.module('locApp.modules.profile.controllers')
          * called the first time nggrid is set up
          * @param {Object} gridApi - the api to register
          */
-        $scope.profileGrid.onRegisterApi = function (gridApi) {
+        $scope.ontologiesGrid.onRegisterApi = function (gridApi) {
             $scope.gridApi = gridApi;
         };
 
@@ -70,20 +78,21 @@ angular.module('locApp.modules.profile.controllers')
          * if neeeded
          */
         $scope.addBlanks = function() {
-            var count = $scope.profiles.length % $scope.numItems;
+            var count = $scope.ontologies.length % $scope.numItems;
 
             // dont add blanks if we dont have to.
-            if(count === 0 && $scope.profiles.length > 0) {
+            if(count === 0 && $scope.ontologies.length > 0) {
                 return;
             }
 
             for(; count < $scope.numItems; count++) {
                 var blank = {
-                    title: "",
-                    description: "",
-                    date: ""
+                    source: "",
+                    'label.__text': "",
+                    oType: "",
+                    'comment.__text': ""
                 };
-                $scope.profiles.push(blank);
+                $scope.ontologies.push(blank);
             }
         };
 
@@ -114,67 +123,4 @@ angular.module('locApp.modules.profile.controllers')
             textBox.value='';
             $scope.addBlanks();
         };
-
-        /**
-         * @ngdoc function
-         * @name sortByParam
-         * @description
-         * Sorts the profile list by the given param and direction. Param must be a valid
-         * key in the Profile object (id, title, description, date, contact
-         * or remark)
-         * @param {String} param - the object key by which to sort
-         * @param {String} direction - whether to sort ascending or descending
-         * @returns {Number} - The result of the comparison which javascript uses for sort functions
-         */
-        $scope.sortByParam = function(param, direction) {
-            $scope.profiles.sort(function(a, b) {
-                var first;
-                var second;
-                //Make sure the data is in the correct form, and that strings
-                //are case insensitive
-                if(param === "date") {
-                    //validate the date parameter
-                    if(!a[param].match('[0-9]{4}-[0-9]{2}-[0-9]{2}')) {
-                        first = '1969-12-31';
-                    }
-                    if(!b[param].match('[0-9]{4}-[0-9]{2}-[0-9]{2}')) {
-                        second = '1969-12-31';
-                    }
-                    first = Date.parse(a[param]);
-                    second = Date.parse(b[param]);
-                } else {
-                    //Move empty fields to the end of the list
-                    if(a[param] === null || a[param] === 'undefined' || a[param] === "" || a[param] === " " || typeof(a[param]) === 'undefined') {
-                        first = "zz";
-                    } else {
-                        first = a[param];
-                    }
-                    if(b[param] === null || b[param] === 'undefined' || b[param] === "" || b[param] === " " || typeof(b[param]) === 'undefined') {
-                        second = "zz";
-                    } else {
-                        second = b[param];
-                    }
-                    first = first.toLowerCase();
-                    second = second.toLowerCase();
-                }
-
-                //Carry out the actual sort function
-                if(direction === 'asc') {
-                    if(first < second) {
-                        return -1;
-                    }
-                    if(first > second) {
-                        return 1;
-                    }
-                } else {
-                    if(first < second) {
-                        return 1;
-                    }
-                    if(first > second) {
-                        return -1;
-                    }
-                }
-                return 0;
-            });
-        };
-    });
+    }); 
