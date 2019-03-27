2b575875a96d41099a223fbc6e571e74f51aec1c
Kirk Hess <kirk.hess@gmail.com>
2019-03-27 18:12:12 +0000

    more sinopia fixes

--- source/assets/js/modules/profile/services/profileHandler.service.js
+++ source/assets/js/modules/profile/services/profileHandler.service.js
@@ -11,8 +11,8 @@ angular.module('locApp.modules.profile.services')
 
         handler.errors = [];
 
-        var profAttributes = ["id","title","description","date","author","contact","remark","adherence","resourceTemplates", "source"];
-        var resAttributes = ["id","resourceURI","resourceURL","resourceLabel","propertyTemplates","author","contact","remark"];
+        var profAttributes = ["id","title","description","date","author","contact","remark","adherence","schema", "resourceTemplates", "source"];
+        var resAttributes = ["id","resourceURI","resourceURL","resourceLabel","propertyTemplates","author", "date","contact","remark", "schema", "adherence","source"];
         var propAttributes = ["propertyURI","propertyLabel","mandatory","repeatable","type","valueConstraint","remark", "resourceTemplates"];
         var consAttributes = ["valueLanguage","languageURI","languageLabel","valueDataType","valueTemplateRefs","useValuesFrom","editable","remark", "repeatable", "defaultURI", "defaultLiteral", "defaults", "validatePattern"];
         var dataAttributes = ["dataTypeURI","dataTypeLabel","dataTypeLabelHint","remark"];
